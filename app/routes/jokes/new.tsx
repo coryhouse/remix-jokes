0bff70dfcf18ea1dcd2372eae9104275501fb8dc
Cory House <housecor@gmail.com>
2021-12-01 10:24:11 -0600

    support adding

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,20 @@
+import { ActionFunction, redirect } from "@remix-run/server-runtime";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error(`Form not submitted correctly.`);
+  }
+  const fields = { name, content };
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
