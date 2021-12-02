a97e51a5c1790172f4ffb440704557d3089d0d01
Cory House <housecor@gmail.com>
2021-12-01 21:36:14 -0600

    add logic

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,18 +1,102 @@
-import type { LinksFunction } from "remix";
+import { ActionFunction, LinksFunction, useActionData } from "remix";
 import { Link, useSearchParams } from "remix";
+import { db } from "~/utils/db.server";
 import stylesUrl from "../styles/login.css";
 
 export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+function validateUsername(username: string) {
+  if (username.length < 4) {
+    return "Usernames must be at least 3 characters long.";
+  }
+}
+
+function validatePassword(password: string) {
+  if (password.length < 6) {
+    return "Passwords must be at least 6 characters long.";
+  }
+}
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    username: string | undefined;
+    password: string | undefined;
+  };
+  fields?: {
+    loginType: string;
+    username: string;
+    password: string;
+  };
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const loginType = form.get("loginType");
+  const username = form.get("username");
+  const password = form.get("password");
+  const redirectTo = form.get("redirectTo");
+  if (
+    typeof loginType !== "string" ||
+    typeof username !== "string" ||
+    typeof password !== "string" ||
+    typeof redirectTo !== "string"
+  ) {
+    return { formError: `Form not submitted correctly.` };
+  }
+
+  const fieldErrors = {
+    name: validateUsername(username),
+    content: validatePassword(password),
+  };
+
+  const fields = { loginType, username, password };
+
+  if (Object.values(fieldErrors).some(Boolean)) return { fieldErrors, fields };
+
+  switch (loginType) {
+    case "login": {
+      // login to get the user
+      // if there's no user, return the fields and a formError
+      // if there is a user, create their session and redirect to /jokes
+      return { fields, formError: "Not implemented" };
+    }
+    case "register": {
+      let userExists = await db.user.findFirst({
+        where: { username },
+      });
+      if (userExists) {
+        return {
+          fields,
+          formError: `User with username ${username} already exists`,
+        };
+      }
+      // create the user
+      // create their session and redirect to /jokes
+      return { fields, formError: "Not implemented" };
+    }
+    default: {
+      return { fields, formError: `Login type invalid` };
+    }
+  }
+};
+
 export default function Login() {
   let [searchParams] = useSearchParams();
+  const actionData = useActionData<ActionData | undefined>();
+
   return (
     <div className="container">
       <div className="content" data-light="">
         <h1>Login</h1>
-        <form method="post">
+        <form
+          method="post"
+          aria-describedby={
+            actionData?.formError ? "form-error-message" : undefined
+          }
+        >
           <input
             type="hidden"
             name="redirectTo"
@@ -25,17 +109,46 @@ export default function Login() {
                 type="radio"
                 name="loginType"
                 value="login"
-                defaultChecked
+                defaultChecked={
+                  !actionData?.fields?.loginType ||
+                  actionData?.fields?.loginType === "login"
+                }
               />{" "}
               Login
             </label>
             <label>
-              <input type="radio" name="loginType" value="register" /> Register
+              <input
+                type="radio"
+                name="loginType"
+                value="register"
+                defaultChecked={actionData?.fields?.loginType === "register"}
+              />{" "}
+              Register
             </label>
           </fieldset>
           <div>
             <label htmlFor="username-input">Username</label>
-            <input type="text" id="username-input" name="username" />
+            <input
+              type="text"
+              id="username-input"
+              name="username"
+              defaultValue={actionData?.fields?.username}
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.username) || undefined
+              }
+              aria-describedby={
+                actionData?.fieldErrors?.username ? "username-error" : undefined
+              }
+            />
+            {actionData?.fieldErrors?.username ? (
+              <p
+                className="form-validation-error"
+                role="alert"
+                id="username-error"
+              >
+                {actionData.fieldErrors.username}
+              </p>
+            ) : null}
           </div>
           <div>
             <label htmlFor="password-input">Password</label>
